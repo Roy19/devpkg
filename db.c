cb273e69ea2f359269563321d4b8c99780d03932
Aritra Sur Roy <aritra1997@hotmail.com>
2018-04-19 00:51:14 +0530

    Add comments for access

--- db.c
+++ db.c
@@ -97,7 +97,8 @@ int DB_init(){
 	apr_pool_initialize();
 	apr_pool_create(&p,NULL);
 
-	if(access(DB_DIR, W_OK | X_OK) == -1){
+	if(access(DB_DIR, W_OK | X_OK) == -1){	// Setup permissions for access to DB_DIR and make 
+						// the directory DB_DIR.
 		apr_status_t rc = apr_dir_make_recursive(DB_DIR, APR_UREAD | 
 				APR_UWRITE | APR_UEXECUTE | 
 				APR_GREAD | APR_GWRITE | APR_GEXECUTE , p);
@@ -105,7 +106,7 @@ int DB_init(){
 	}
 	if(access(DB_FILE, W_OK) == -1){
 		FILE * db = DB_open(DB_FILE, "w");
-		check(db, "Cannot open database file: %s", DB_FILE);
+		check(db, "Cannot create database file: %s", DB_FILE);
 		DB_close(db);
 	}
 
